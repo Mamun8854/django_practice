018084dc3fd8965f52753c0094d7f7682267d70e
Abdullah Al Mamun <mamunabdullah8854@gmail.com>
2023-08-17 11:29:47 +0600

    solve migration error and insert data to db using admin.py file

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from home.models import Student
 # Register your models here.
+@admin.register(Student)
+class studentModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "email","address","mobile"]
--- home/models.py
+++ home/models.py
@@ -1,13 +1,7 @@
 from django.db import models
-# Create your models here.
-class Student(models.Model):
-    name = models.CharField(max_length = 100),
-    age = models.IntegerField(),
-    contact = models.IntegerField(),
-    email = models.EmailField(),
-    address = models.TextField(),
 
-class tiger_park(models.Model):
-    name = models.CharField(max_length=100),
-    total_eployee = models.IntegerField(),
-    clients = models.TextField()
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    mobile = models.IntegerField(default=0)
